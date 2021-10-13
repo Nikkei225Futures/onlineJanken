1b362e6253b0ad67d24157d7775c2399de7c5616
Nikkei225Futures <230373g@ugs.kochi-tech.ac.jp>
2021-10-13 23:54:46 +0900

    first commit

---
+++ client/js/client.js
@@ -0,0 +1,158 @@
+let svr = new WebSocket('ws://localhost:8888');
+
+function createConnection(){
+    //delete submitButton to avoid duplication
+    let submitButton = document.getElementById('nameSubmitBtn');
+    submitButton.setAttribute("style", "display: none;");
+
+    //send request of register
+    let name = document.getElementById('name').value;
+    let connectionReq = 
+    {
+        "jsonrpc": "2.0",
+        "method": "join",
+        "name": `${name}`
+    };
+
+    svr.send(JSON.stringify(connectionReq));
+    console.log(`connectionRequest`);
+
+}
+
+function decideHand(hand){
+    //user can decide their hand when they have already registered name
+    let submitButton = document.getElementById("nameSubmitBtn");
+    if(submitButton.hasAttribute("style")){
+        let name = document.getElementById('name').value;
+        let handReq = 
+        {
+            "jsonrpc": "2.0",
+            "method": "decide",
+            "name": `${name}`,
+            "hand": `${hand}`
+        };
+    
+        svr.send(JSON.stringify(handReq));
+        console.log(`decisionRequest`);
+    }else{
+        addLog("register your name before playing");
+    }
+}
+
+//when events happened
+svr.addEventListener('open', () => {
+    console.log('socket opened');
+});
+
+svr.addEventListener('message', msg => {
+    msg = msg.data;
+    //console.log(msg);
+    msg = JSON.parse(msg);
+
+    if(msg.method == "join"){
+        console.log("person joined: " + msg.name);
+        addLog(`${msg.name} joined the game`);
+    }else if(msg.method == "leave"){
+        console.log("person left: " + msg.name);
+        addLog(`${msg.name} left the game`);
+    }else if(msg.method == "status"){
+        showStatus(msg.names, msg.status);
+        console.log("status");
+        console.log(msg);
+    }else if(msg.method == "result"){
+        showResult(msg.names, msg.hands, msg.settle, msg.winners);
+        console.log("result:");
+        console.log(msg);
+    }
+});
+
+function showResult(names, hands, settle, winners){
+    //delete prev data first
+    let parent = document.getElementById("prevStats");
+    parent.innerHTML = "";
+
+    //update result
+    for(let i = 0; i < names.length; i++){
+        let newParagraph = document.createElement("pre");
+        let sentence = document.createTextNode("loading");
+        newParagraph.appendChild(sentence);
+        newParagraph.setAttribute("class", "results");
+        parent.insertBefore(newParagraph, parent.firstChild);  
+    }
+
+    let resultLists = document.getElementsByClassName("results");
+    for(let i = 0; i < names.length; i++){
+        resultLists[i].innerHTML = names[i] + ":&#009;" + hands[i];
+    }
+
+    let resultTitle = document.getElementById("prevResultTitle");
+    if(settle == "draw"){
+        resultTitle.innerHTML = "previous result: draw";
+    }else{
+        let winnerStr = "";
+        for(let i = 0; i < winners.length; i++){
+            winnerStr += winners[i];
+            if(i != winners.length-1){
+                winnerStr += ", ";
+            }
+        }
+        resultTitle.innerHTML = "previous result: " + winnerStr + " won the game";
+    }
+
+}
+
+function showStatus(names, status){
+    //delete prev data first
+    let parent = document.getElementById("status");
+    parent.innerHTML = "";
+
+    for(let i = 0; i < names.length; i++){
+        let newParagraph = document.createElement("pre");
+        let sentence = document.createTextNode("loading");
+        newParagraph.appendChild(sentence);
+
+        newParagraph.setAttribute("class", "person");
+    
+        parent.insertBefore(newParagraph, parent.firstChild);  
+    }
+
+    let statusLists = document.getElementsByClassName("person");
+    for(let i = 0; i < names.length; i++){
+        statusLists[i].innerHTML = names[i] + ":&#009;" + status[i];
+    }
+}
+
+function addLog(message){
+    let logs = document.getElementsByClassName("log");
+    //rotate the logs
+    for(let i = logs.length-1; i > 0; i--){
+        if(logs[i-1].innerHTML != null){
+            logs[i].innerHTML = logs[i-1].innerHTML;
+        }
+    }
+    
+    logs[0].innerHTML = message;
+}
+
+
+
+//close
+function closeConnection(){
+    let name = document.getElementById('name').value;
+    let leaveReq = 
+    {
+        "jsonrpc": "2.0",
+        "method": "leave",
+        "name": `${name}`
+    };
+
+    svr.send(JSON.stringify(leaveReq));
+    console.log(`leaveRequest`);
+    console.log(leaveReq);
+
+    svr.close(3005);
+}
+
+window.onunload = function(){
+    closeConnection();
+}
---
+++ server/server.js
@@ -0,0 +1,252 @@
+class Person{
+    constructor(name){
+        this.name = name;
+        this.hand = "none";
+    }
+}
+
+const server = require('ws').Server;
+const ws = new server({port: 8888});
+let person = [];
+
+ws.on('connection', socket => {
+    console.log('connected');
+
+    socket.on('message', msg => {
+        msg = JSON.parse(msg);
+
+        if(msg.method == "join"){
+            addPerson(msg.name);
+
+        }else if(msg.method == "decide"){
+            console.log(`decide hand(${msg.name}): ${msg.hand}`);
+            setHand(msg.name, msg.hand);
+            bcCurrentStat();
+            if(isAllReady()){
+                console.log("all ready");
+                bcAllHands();
+            }
+
+        }else if(msg.method == "leave"){
+            deletePerson(msg.name);
+            bcCurrentStat();
+        }
+
+    });
+
+    socket.on('close', () => {
+        console.log('byebye');
+    });
+
+});
+
+function addPerson(name){
+    console.log("person joined: " + name);
+    //add person @ tail
+    person.push(new Person(name));
+
+    let joinRes =
+    {
+        "jsonrpc": "2.0",
+        "method": "join",
+        "name": `${name}`
+    };
+
+    //broadcast the msg about person joined
+    ws.clients.forEach(client => {
+        client.send(JSON.stringify(joinRes));
+    });
+    debug();
+}
+
+function deletePerson(name){
+    console.log("person left: " + name);
+    //delete personData which is matched to request
+    for(let i = 0; i < person.length; i++){
+        if(person[i].name == name){
+            person.splice(i, 1);
+            break;
+        }
+    }
+
+    let leaveRes =
+    {
+        "jsonrpc": "2.0",
+        "method": "leave",
+        "name": `${name}`
+    };    
+    
+    //broadcast the msg about person left
+    ws.clients.forEach(client => {
+        client.send(JSON.stringify(leaveRes));
+    });
+    debug();
+}
+
+function setHand(name, hand){
+    for(let i = 0; i < person.length; i++){
+        if(person[i].name == name){
+            person[i].hand = hand;
+            break;
+        }
+    }
+}
+
+//bc = broadcast
+function bcCurrentStat(){
+    let msg =
+    {
+        "jsonrpc": "2.0",
+        "method": "status",
+        "names": [],       
+        "status": []        //"ready" or "voting"
+    }
+
+    for(let i = 0; i < person.length; i++){
+        msg.names.push(person[i].name);
+        //if ready
+        if(person[i].hand != "none"){
+            msg.status.push("ready");
+        }else if(person[i].hand == "none"){
+            msg.status.push("voting");
+        }else{
+            console.log(`invalid value @ person[${i}].hand`);
+        }
+    }
+
+    console.log(msg);
+
+    ws.clients.forEach(client => {
+        client.send(JSON.stringify(msg));
+    });
+
+}
+
+//broadcast result
+function bcAllHands(){
+    let msg =
+    {
+        "jsonrpc": "2.0",
+        "method": "result",
+        "names": [],       
+        "hands": [],        //"ready" or "voting"
+        "settle": "draw",
+        "winners": []
+    }
+    
+    for(let i = 0; i < person.length; i++){
+        msg.names.push(person[i].name);
+        msg.hands.push(person[i].hand);
+    }
+
+    let winners = judge();
+    if(winners == false){
+        msg.settle = "draw";
+    }else{
+        msg.settle = "settled";
+        for(let i = 0; i < winners.length; i++){
+            msg.winners.push(winners[i]);
+        }
+    }
+
+    console.log(msg);
+
+    ws.clients.forEach(client => {
+        client.send(JSON.stringify(msg));
+    });
+
+    initHands();
+}
+
+function isAllReady(){
+    for(let i = 0; i < person.length; i++){
+        if(person[i].hand == "none"){
+            return false;
+        }
+    }
+    return true;
+}
+
+function judge(){
+    if(person.length >= 2){
+        let result = isSettled();
+        if(result == false){
+            return false;
+        }else{
+            let rocks = result[0];
+            let scissors = result[1];
+            let papers = result[2];
+            console.log("rocks: " + rocks);
+            console.log("scissors: " + scissors);
+            console.log("papers: " + papers);
+            let winners = [];
+            let rockPpl = [];
+            let scissorPpl = [];
+            let paperPpl = [];
+
+            for(let i = 0; i < person.length; i++){
+                if(person[i].hand == "rock"){
+                    rockPpl.push(person[i].name);
+                }else if(person[i].hand == "scissor"){
+                    scissorPpl.push(person[i].name);
+                }else if(person[i].hand == "paper"){
+                    paperPpl.push(person[i].name);
+                }
+            }
+
+
+            if(rocks == 0){
+                winners = scissorPpl;
+            }else if(scissors == 0){
+                winners = paperPpl;
+            }else if(papers == 0){
+                winners = rockPpl;
+            }
+            console.log("winners: " + winners);
+            return winners;
+        }
+    }else{
+        return false;
+    }
+}
+
+//settled -> true, draw -> false
+function isSettled(){
+    let rocks = 0;
+    let scissors = 0;
+    let papers = 0;
+    for(let i = 0; i < person.length; i++){
+        if(person[i].hand == "rock"){
+            rocks++;
+        }else if(person[i].hand == "scissor"){
+            scissors++;
+        }else if(person[i].hand == "paper"){
+            papers++;
+        }
+    }
+
+    //when all hands appear
+    if(rocks != 0 && scissors != 0 && papers != 0){
+        return false;
+    //all ppl take same hands
+    }else if(rocks == person.length || scissors == person.length || papers == person.length){
+        return false;
+    }else{
+        console.log([rocks, scissors, papers]);
+        return [rocks, scissors, papers];
+    }
+}
+
+
+function debug(){
+    for(let i = 0; i < person.length; i++){
+        console.log(person[i].name);
+    }
+}
+
+function initHands(){
+    for(let i = 0; i < person.length; i++){
+        person[i].hand = "none";
+    }
+    bcCurrentStat();
+}
